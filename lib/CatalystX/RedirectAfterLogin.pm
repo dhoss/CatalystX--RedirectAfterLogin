04ffacffb7582a3539edab986439f9f1ae79208d
Devin Austin <devin@devin-laptop.(none)>
2009-07-12 21:37:12 -0600

    Added redirect code

--- lib/CatalystX/RedirectAfterLogin.pm
+++ lib/CatalystX/RedirectAfterLogin.pm
@@ -1,5 +1,27 @@
 package CatalystX::RedirectAfterLogin;
 
 use Moose::Role;
+use namespace::autoclean;
+
+## pretty much just taken from my post here:
+## http://dev.catalystframework.org/wiki/wikicookbook/nextpageredirect
+sub next_page {
+    my ($c) = shift;
+
+    my $redirect_url;
+    if ( defined $c->flash->{next_page} ) {
+
+        $redirect_url = $c->uri_for( $c->flash->{next_page} );
+
+    } else {
+
+        $redirect_url = $c->uri_for( '/' );
+
+    }
+
+    $c->res->redirect( $redirect_url );
+
+}
+
 
 1;
